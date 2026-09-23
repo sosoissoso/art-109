207cd9181d3d069be0d18265abba55ca03d008d0
sosoissoso <146895653+sosoissoso@users.noreply.github.com>
2026-09-23 03:11:52 -0700

    finished tut8

---
+++ tutorials/2-css/03-css-port-grid/js/hamburger.js
@@ -0,0 +1,11 @@
+//console.log("bleh bleh");
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+
+
+})
